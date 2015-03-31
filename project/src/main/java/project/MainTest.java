565f6d8081541334ed5dba9c97f3b887a7f37709
John Heng <john@thaismartcard.co.th>
2015-03-31 15:48:08 +0700

    test commit 1

---
+++ project/src/main/java/project/MainTest.java
@@ -0,0 +1,13 @@
+package project;
+
+import com.itextpdf.text.Document;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Document doc = new Document();
+		
+		System.out.println("test");
+	}
+
+}
