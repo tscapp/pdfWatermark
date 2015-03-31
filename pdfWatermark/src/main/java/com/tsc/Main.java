69337814816761fe3f4651db621ed82805d1a6f7
John Heng <john@thaismartcard.co.th>
2015-03-31 16:46:09 +0700

    1. revise addWaterMark 2. add Method overloading to addWaterMark

--- pdfWatermark/src/main/java/com/tsc/Main.java
+++ pdfWatermark/src/main/java/com/tsc/Main.java
@@ -6,10 +6,14 @@ import com.itextpdf.text.DocumentException;
 
 public class Main {
 
-	public static void main(String[] args) throws DocumentException, IOException {
+	public static void main(String[] args) {
 		PdfWaterMark pdfwatermark = new PdfWaterMark();
-		pdfwatermark.createPdf("test.pdf");
-		
+		try {
+			pdfwatermark.addWaterMark("บริษัท กรุงเทพมหานครอมรรัตรโกสินทร์มหินทรายุธยามหาดิลกภพนพรัตนราชธานีบูรีรมย์", "20150315", "John Heng");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		} 
+		System.out.println("test");
 	}
-
 }
--- pdfWatermark/src/main/java/com/tsc/PdfWaterMark.java
+++ pdfWatermark/src/main/java/com/tsc/PdfWaterMark.java
@@ -19,8 +19,10 @@ import com.sun.prism.paint.Color;
 
 
 public class PdfWaterMark {
-	public void addWaterMark() throws IOException, DocumentException {
+	public void addWaterMark(String sPartnerName, String sDate, String sUserName) throws IOException, DocumentException {
+    	//Read Source Pdf file
     	PdfReader reader = new PdfReader("contract.pdf");
+    	//Retrieve number of pages from the file
         int n = reader.getNumberOfPages();
 
         // Create a stamper that will copy the document to a new file
@@ -28,20 +30,54 @@ public class PdfWaterMark {
         int i = 1;
         PdfContentByte cb;
         BaseFont bf = BaseFont.createFont("THSarabunNew.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        while (i < n) 
-        {
+        while (i < n)         {
 			// Text under the existing page
 			cb = stamp.getUnderContent(i);
+			//Set font opacity
 			PdfGState pdfgs = new PdfGState();
 			pdfgs.setFillOpacity(0.4f);
 			cb.setGState(pdfgs);
-			
+			//Set text rotation
 			int rotate = 55;
 	        cb.beginText();
 	        cb.setFontAndSize(bf, 50);
-            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "Confidential 20150329", 50, 200, rotate);
-            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "บริษัท กรุงเทพมหานครอมรรัตรโกสินทร์มหินทรายุธยามหาดิลกภพนพรัตนราชธานีบูรีรมย์", 70, 100, rotate);
-            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "John Heng", 200, 150, rotate);
+            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "Confidential " + sDate, 50, 200, rotate);
+            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, sPartnerName, 70, 100, rotate);
+            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, sUserName, 200, 150, rotate);
+	        cb.endText();			
+						
+			i++;
+        }
+      
+        stamp.close();    	
+        System.out.println("Watermark completed");
+    }
+	
+	public void addWaterMark(String sPartnerName, String sDate, String sUserName, String sourceFile, String destFile) throws IOException, DocumentException {
+    	//Read Source Pdf file
+    	PdfReader reader = new PdfReader(sourceFile);
+    	//Retrieve number of pages from the file
+        int n = reader.getNumberOfPages();
+
+        // Create a stamper that will copy the document to a new file
+        PdfStamper stamp = new PdfStamper(reader,  new FileOutputStream(destFile));
+        int i = 1;
+        PdfContentByte cb;
+        BaseFont bf = BaseFont.createFont("THSarabunNew.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        while (i < n)         {
+			// Text under the existing page
+			cb = stamp.getUnderContent(i);
+			//Set font opacity
+			PdfGState pdfgs = new PdfGState();
+			pdfgs.setFillOpacity(0.4f);
+			cb.setGState(pdfgs);
+			//Set text rotation
+			int rotate = 55;
+	        cb.beginText();
+	        cb.setFontAndSize(bf, 50);
+            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "Confidential " + sDate, 50, 200, rotate);
+            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, sPartnerName, 70, 100, rotate);
+            cb.showTextAligned(PdfContentByte.ALIGN_LEFT, sUserName, 200, 150, rotate);
 	        cb.endText();			
 						
 			i++;
